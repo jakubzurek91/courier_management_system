108106e8cbbad3751909e2f5977cc1eae1eec5be
Uzytkownik <uzytkownik@kursGIT.pl>
2020-09-05 15:59:19 +0200

    dodanie klucza obcego do klasy Client

--- cms/models.py
+++ cms/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.forms import CharField
 
 
-class Parcel(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=20)
-    address = models.CharField(max_length=30)
-    zip_code = models.CharField(max_length=6)
-    phone_nb = models.IntegerField()
-
-
 class Client(models.Model):
-    parcel_id = models.ForeignKey(Parcel, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=30)
     zip_code = models.CharField(max_length=6)
     phone_nb = models.IntegerField()
     email = models.CharField(max_length=30)
 
+
+class Parcel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=6)
+    phone_nb = models.IntegerField()
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
