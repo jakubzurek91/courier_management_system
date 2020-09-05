77b074e7ff8a4f9be8d7c376a9e31ffd54959439
Jakub <Jacobsen@poczta.fm>
2020-09-05 10:49:32 +0200

    add models

---
+++ modles.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.forms import CharField
+
+
+class Parcel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=30)
+    zip_code = models.CharField(max_length=6)
+    phone_nb = models.IntegerField()
+
+class Client(models.Model):
+    id = models.ForeignKey()
